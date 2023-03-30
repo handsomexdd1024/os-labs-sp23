98e5b3d57d24d4e225eb43f16acb19c235b0827b
Billy Yang <me@motorscooter.run>
2023-03-30 15:23:09 +0800

    bugfix

--- lab01/exec.c
+++ lab01/exec.c
@@ -4,15 +4,19 @@
 #include <assert.h>
 
 int main(int argc, char **argv) {
-    pid_t pid, status;
+    pid_t pid;
+    int status;
     pid = fork();
     assert(pid >= 0);
     if (pid == 0) {
         printf("Into sub-process\n");
         execlp("neofetch", "");
+        printf("Execution failed\n");
+        return 1;
     } else {
-        printf("Into master process");
+        printf("Into master process\n");
         wait(&status);
+        printf("Sub-process returned %d\n", status);
     }
     return 0;
 }
--- lab01/pid.c
+++ lab01/pid.c
@@ -9,17 +9,17 @@ int main(int argc, char** argv) {
     assert(pid >= 0);
     if (pid > 0) {
         pid = fork();
+        if (pid > 0) {
+            printf("aaaaaaaa");
+            pid_t child_status;
+            wait(&child_status);
+            wait(&child_status);
+            printf("\n");
+        } else {
+            printf("cccccccc");
+        }
     } else {
         printf("bbbbbbbb");
-    }
-    if(pid > 0) {
-        printf("aaaaaaaa");
-        pid_t child_status;
-        wait(&child_status);
-        wait(&child_status);
-        printf("\n");
-    } else {
-        printf("cccccccc");
     }
     return 0;
 }
